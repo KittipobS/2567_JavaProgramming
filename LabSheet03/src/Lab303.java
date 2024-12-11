fe6916c695f85974deb9ef13d9927031520a6d95
KittipobS <kittipobtwo@gmail.com>
2024-12-11 11:18:32 +0700

    [lab03]

--- LabSheet03/src/Lab303.java
+++ LabSheet03/src/Lab303.java
@@ -1,9 +1,37 @@
+import java.util.*;
 
 public class Lab303 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		
+		String sentence = "";
+		int space = 0;
+		int word = 0;
+		
+		System.out.print("Input a sentence : ");
+		sentence = scan.nextLine();
+		
+		while (!sentence.endsWith(".")) {
+			System.out.print("Input a sentence, again : ");
+			sentence = scan.nextLine();
+		}
+		for (int i = 0; i < sentence.length(); i++) {
+			char ch = sentence.charAt(i);
+			
+			if (ch == ' ') {
+				space++;
+			}else {
+				word++;
+			}
+			
+			
+		}
+		System.out.println("\nThis sentence has " + space + " spacebar.");
+		System.out.println("This sentence has " + word + " word.");
+		
+		scan.close();
 	}
 
 }
