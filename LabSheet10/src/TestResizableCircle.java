ba01504a475633e5a75185955221a61dc88e51fa
KittipobS <kittipobtwo@gmail.com>
2025-02-19 10:36:32 +0700

    [Ex10]

---
+++ LabSheet10/src/TestResizableCircle.java
@@ -0,0 +1,24 @@
+
+public class TestResizableCircle {
+
+	public static void main(String[] args) {
+		ResizableCircle rc = new ResizableCircle(10.0);
+		
+		System.out.println(rc);
+		System.out.println("Perimeter: " + rc.getPerimeter());
+		System.out.println("Area: " + rc.getArea());
+		Line();
+		rc.resize(50);
+		System.out.println("After resize(50%): " + rc);
+		System.out.println("Perimeter: " + rc.getPerimeter());
+		System.out.println("Area: " + rc.getArea());
+		
+	}
+	
+	public static void Line() {
+		for (int i = 1; i <= 50; i++)
+			System.out.print("*");
+		System.out.println();
+	}
+
+}
